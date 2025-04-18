64f77f26ef80711ce2349ebf164f390726187147
Daniel Ribeiro Medeiros <kabal79@gmail.com>
2025-04-18 10:36:13 -0300

    Fix tests

--- src/shared/infrastructure/env-config/__tests__/unit/env-config.service.spec.ts
+++ src/shared/infrastructure/env-config/__tests__/unit/env-config.service.spec.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 
 describe('EnvConfigService', () => {
     let service: EnvConfigService;
+    let configService: ConfigService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -12,16 +13,44 @@ describe('EnvConfigService', () => {
                 {
                     provide: ConfigService,
                     useValue: {
-                        get: jest.fn(), // Mock do ConfigService
+                        get: jest.fn().mockImplementation((key: string) => {
+                            if (key === 'PORT') {
+                                return '3000'; // Ou poderia ser 3000 diretamente
+                            }
+                            return null;
+                        }),
                     },
                 },
             ],
         }).compile();
 
         service = module.get<EnvConfigService>(EnvConfigService);
+        configService = module.get<ConfigService>(ConfigService);
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('should return the variable PORT', () => {
+        // Opcional: você pode adicionar esta verificação para garantir que o mock está sendo chamado
+        jest.spyOn(configService, 'get').mockReturnValue('3000');
+
+        const port = service.getAppPort();
+        expect(port).toBe(3000);
+        // Opcional: verificar se o método get foi chamado com o parâmetro correto
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        expect(configService.get).toHaveBeenCalledWith('PORT');
+    });
+
+    it('should return the variable NODE_ENV', () => {
+        // Opcional: você pode adicionar esta verificação para garantir que o mock está sendo chamado
+        jest.spyOn(configService, 'get').mockReturnValue('test');
+
+        const nodeENV = service.getNodeEnv();
+        expect(nodeENV).toBe('test');
+        // Opcional: verificar se o método get foi chamado com o parâmetro correto
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        expect(configService.get).toHaveBeenCalledWith('NODE_ENV');
     });
 });
